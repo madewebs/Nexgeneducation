26ef6e1c801c9b3704adba4e5c251b7eff5d98e0
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2026-06-02 13:03:03 +0530

    error page style

--- src/pages/Error/Page.jsx
+++ src/pages/Error/Page.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   return (
-    <div className="flex items-center justify-center w-full min-h-screen text-6xl text-white bg-black">
-      <p className="items-center text-center ">
-        404
-        <br />
-        <span className="text-2xl">Page Not Found</span>
-      </p>
+    <div className="flex flex-col items-center justify-center w-full min-h-screen text-6xl text-[#232f65]">
+      <div>
+          <p className="items-center text-4xl text-center">404</p>
+          <p className="text-2xl">Page Not Found</p>
+      </div>
+      <div  className="mt-4 p-4 bg-[#232f65] text-[#fefefe] rounded-xl ">
+        <Link to="/">Return to Home page</Link>
+      </div>
     </div>
   );
 }
